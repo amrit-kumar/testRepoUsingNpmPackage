f90e064b9b32fb6a2a32bea678c87297f2cc68a4
Amrit Kumar <kumaramrit38@gmail.com>
2021-04-03 11:09:49 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import Table from './Table'
 
 class App extends Component {
   render() {
+    const characters = [
+        {
+          name: 'Charlie',
+          job: 'Janitor',
+        },
+        {
+          name: 'Mac',
+          job: 'Bouncer',
+        },
+        {
+          name: 'Dee',
+          job: 'Aspring actress',
+        },
+        {
+          name: 'Dennis',
+          job: 'Bartender',
+        },
+      ]
+
     return (
       <div className="App">
         <h1>Hello, React!</h1>
-        <Table />
+        <div className="container">
+    <Table characterData={characters} />
+  </div>
       </div>
 
     )
